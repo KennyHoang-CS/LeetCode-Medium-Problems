2b4fc2fd8dd33e0b63ce95f0123f44886874ffc8
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-10-02 01:52:50 -0700

    string exercise

---
+++ Strings/longestSubStringWithoutRepeats.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+var lengthOfLongestSubstring = function(s) {
+    let start = 0, max = 0;
+    let map = new Map(); 
+    
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (map.get(char) >= start) start = map.get(char) + 1;
+        map.set(char, i);  
+        max = Math.max(max, i - start + 1);
+    }
+    
+    return max;
+};
